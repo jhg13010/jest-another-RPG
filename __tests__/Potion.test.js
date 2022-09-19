5942c77455c836f0f132795794fa73589ee79e90
Jarrett Gray <jarrett.gray@uconn.edu>
2022-09-19 14:32:05 -0600

    created 2 potion tests and refactored code with passing results

---
+++ __tests__/Potion.test.js
@@ -0,0 +1,18 @@
+const Potion = require('../lib/Potion.js');
+
+test('creates a health potion object', () => {
+    //passes 'health' as a parameter into the Potion function from Potion.js 
+    const potion = new Potion('health');
+
+    //expected outputs
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+});
+
+test('creates a random potion object', () => {
+    const potion = new Potion();
+
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
+    expect(potion.value).toEqual(expect.any(Number));
+});
